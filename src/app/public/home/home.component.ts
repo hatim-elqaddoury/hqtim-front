3e89159a3b5d0e4a64120518210bb9f8330d5728
hatim-elqaddoury <hatiimk@gmail.com>
2021-05-14 23:58:48 +0200

    apply some modifs

--- src/app/public/home/home.component.ts
+++ src/app/public/home/home.component.ts
@@ -1,10 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
-import { title } from '../../@core/mock/conf'
-import { CookiesService } from '../../@core/utils/cookies.service';
-import { HqtimApiService } from '../../@core/utils/hqtim-api.service';
+import { title } from '../../@core/mock/conf';
 
-declare let paypal: any;
 
 @Component({
   selector: 'hq-home',
@@ -13,89 +10,19 @@ declare let paypal: any;
 })
 export class HomeComponent implements OnInit {
 
-  username: any = this.cookieService.get("username");
-  objectt: any = this.cookieService.getObject("object");
-  ip:any = this.cookieService.get("ip");
-  finalAmount: number = 1;
-
   constructor(
     private titleService: Title,
-    private cookieService: CookiesService,
-    private hqtimApi: HqtimApiService,
-    ) {
-      // console.error(this.username);
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.setPageTitle();
   }
 
   setPageTitle(): void {
     let page = this.titleService['_doc']['URL'].split('/').filter(Boolean).pop();
     page = page.charAt(0).toUpperCase() + page.slice(1);
     this.titleService['_doc']['title'] = page + "・" + title.value;
-  }
-
-  ngOnInit(): void {
-    this.setPageTitle();
-    this.getProducts();
-    
-    // this.ConfigPaypal();
-  }
-
-  products: any= []
-  getProducts(){
-    this.hqtimApi.getProducts().subscribe(
-      (res)=>{
-        this.products=res;
-      },
-      (err)=>{
-        console.log(err);
-      }
-    )
-  }
-
-
-
-  ConfigPaypal() {
-    this.addPaypalScript().then((res) => {
-      paypal.Buttons({
-        locale: 'en_US',
-        brand_name: "HQtim",
-        style: {
-          size: 'small',
-          layout: 'vertical',
-          color: 'silver',
-          shape: 'pill',
-          label: 'pay'
-        },
-        createOrder: (data, actions) => {
-          return actions.order.create({
-            //intent: "CAPTURE",
-            brand_name: "hqapp2",
-            reference_id: "PUHF",
-            purchase_units: [
-              {
-                brand_name: "hqapp2",
-                reference_id: "PUHF",
-                description: "Sporting Goods",
-
-                custom_id: "CUST-HighFashions",
-                soft_descriptor: "HighFashions",
-                amount: {
-                  value: this.finalAmount,
-                }
-              }
-            ]
-          });
-        }
-      }).render("#PaypalCheckout");
-    });
-  }
-
-  addPaypalScript() {
-    return new Promise<any>((resolve, reject) => {
-      let ScriptTagElement = document.createElement('script');
-      ScriptTagElement.src = "https://www.paypal.com/sdk/js?client-id=" + "Acx3Y78SKuq2KBY3g4wGUDNRE2A6LoUEjZzOeqEHz1vREusHZxagaxfpefpppiRsVZxu9h-1Gc0ADQ14" + "&currency=EUR";
-      ScriptTagElement.onload = resolve;
-      document.body.appendChild(ScriptTagElement);
-    })
   }
 
 
--- src/app/public/shopping/shopping.component.ts
+++ src/app/public/shopping/shopping.component.ts
@@ -1,16 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { title } from '../../@core/mock/conf';
+import { CookiesService } from '../../@core/utils/cookies.service';
+import { HqtimApiService } from '../../@core/utils/hqtim-api.service';
+
+declare let paypal: any;
 
 @Component({
   selector: 'hq-shopping',
   templateUrl: './shopping.component.html',
-  styleUrls: ['./shopping.component.scss', './shopping.animation.scss']
+  styleUrls: ['./shopping.component.scss']
 })
 export class ShoppingComponent implements OnInit {
 
+  username: any = this.cookieService.get("username");
+  objectt: any = this.cookieService.getObject("object");
+  ip: any = this.cookieService.get("ip");
+  finalAmount: number = 1;
+
   constructor(
-    private titleService: Title,) {
+    private titleService: Title,
+    private cookieService: CookiesService,
+    private hqtimApi: HqtimApiService,
+  ) {
+    // console.error(this.username);
   }
 
   setPageTitle(): void {
@@ -21,17 +34,68 @@ export class ShoppingComponent implements OnInit {
 
   ngOnInit(): void {
     this.setPageTitle();
-    const counter = document.querySelector(".counter");
-    let count = 0;
-    setInterval(() => {
-      if (count == 92) {
-        clearInterval(count);
-      } else {
-        count += 1;
-        counter.textContent = count + "%";
-      }
-    }, 42);
+    this.getProducts();
 
+    // this.ConfigPaypal();
+  }
+
+  products: any = []
+  getProducts() {
+    this.hqtimApi.getProducts().subscribe(
+      (res) => {
+        this.products = res;
+      },
+      (err) => {
+        console.log(err);
+      }
+    )
+  }
+
+
+
+  ConfigPaypal() {
+    this.addPaypalScript().then((res) => {
+      paypal.Buttons({
+        locale: 'en_US',
+        brand_name: "HQtim",
+        style: {
+          size: 'small',
+          layout: 'vertical',
+          color: 'silver',
+          shape: 'pill',
+          label: 'pay'
+        },
+        createOrder: (data, actions) => {
+          return actions.order.create({
+            //intent: "CAPTURE",
+            brand_name: "hqapp2",
+            reference_id: "PUHF",
+            purchase_units: [
+              {
+                brand_name: "hqapp2",
+                reference_id: "PUHF",
+                description: "Sporting Goods",
+
+                custom_id: "CUST-HighFashions",
+                soft_descriptor: "HighFashions",
+                amount: {
+                  value: this.finalAmount,
+                }
+              }
+            ]
+          });
+        }
+      }).render("#PaypalCheckout");
+    });
+  }
+
+  addPaypalScript() {
+    return new Promise<any>((resolve, reject) => {
+      let ScriptTagElement = document.createElement('script');
+      ScriptTagElement.src = "https://www.paypal.com/sdk/js?client-id=" + "Acx3Y78SKuq2KBY3g4wGUDNRE2A6LoUEjZzOeqEHz1vREusHZxagaxfpefpppiRsVZxu9h-1Gc0ADQ14" + "&currency=EUR";
+      ScriptTagElement.onload = resolve;
+      document.body.appendChild(ScriptTagElement);
+    })
   }
 
 }
